ca360e839a3b2fabcdcfa09c2b1a5953f52ad0db
Armlockx <reus_julio@hotmail.com>
2026-06-23 10:45:28 -0300

    feat: add constant component for images directory

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,10 @@
 export const GUEST_COOKIE = "guest_session_id";
+
+export const STORAGE_BASE = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/RestaurantAI`;
+
+export const FAVICON_URL = `${STORAGE_BASE}/misc/favicon.gif`;
+export const AI_ICON_URL = `${STORAGE_BASE}/misc/ai_icon.png`;
+
+export function menuItemImageUrl(filename: string): string {
+  return `${STORAGE_BASE}/itens/${filename}`;
+}
